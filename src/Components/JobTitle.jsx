722cb66994912db684562d164c824d262fafdba8
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-02-23 23:15:52 +0100

    JobListing section completed

---
+++ src/Components/JobTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JobTitle = ({title}) => {
+  return (
+    <div className="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">
+        {title}
+    </div>
+  )
+}
+
+export default JobTitle
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ShowCase from '../Components/ShowCase'
 import TopBanner from '../Components/TopBanner'
+import JobListing from '../Components/jobs/JobListing'
 
 const HomePage = () => {
   return (
     <div>
       <ShowCase/>
       <TopBanner/>
+      <JobListing/>
     </div>
   )
 }
