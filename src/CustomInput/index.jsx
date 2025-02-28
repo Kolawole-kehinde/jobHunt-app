18b90248855014e40739e9f8be52b9750701727b
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-02-28 22:46:53 +0100

    CustomInput created

---
+++ src/CustomInput/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CustomInput = ({label, type, name,  placeholder, className, ...res}) => {
+  return (
+    <div className="mb-4">
+          {
+            label && <label htmlFor={name}></label>
+          }
+           <div>
+           <input
+            type={type || "text"}
+            name={name}
+            placeholder={placeholde}
+            className={`w-full px-4 py-2 border rounded focus:outline-none ${className}`}
+           { ...res}
+          />
+           </div>
+        </div>
+  )
+}
+
+export default CustomInput;
