408fd9b261597436305855aea2d602cb4f784113
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-03-23 23:08:42 +0100

    integrating supabase for my application

--- src/Components/jobs/JobListing.jsx
+++ src/Components/jobs/JobListing.jsx
@@ -3,8 +3,34 @@ import { FaArrowAltCircleRight } from "react-icons/fa";
 import Job from "./Job";
 import { jobListings } from "../../constant/job";
 import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import { supabase } from "../../libs/supabase";
 
-const JobListings = ({title="Recent Jobs", ShowAllJobsBtn=true}) => {
+
+const JobListings = ({ title = "Recent Jobs", ShowAllJobsBtn = true }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getJobs = async () => {
+      setLoading(true);
+      try {
+        const { data, error } = await supabase.from("jobs").select("*");
+        if (error) {
+          throw error;
+        }
+        setData(data);
+      } catch (error) {
+        console.log('error', error);
+      } finally{
+        setLoading(false);
+      }
+    };
+    getJobs();
+  }, []);
+
+  console.log(data, loading);
+
   return (
     <section>
       <div className="container mx-auto p-4 mt-4">
@@ -15,18 +41,17 @@ const JobListings = ({title="Recent Jobs", ShowAllJobsBtn=true}) => {
             <Job key={job.id} {...job} />
           ))}
         </div>
-        {
-          ShowAllJobsBtn && (
-
-            <div className="flex justify-center items-center">
-              <Link to="/jobs" className="flex items-center gap-1 text-xl text-center">
-                <FaArrowAltCircleRight />
-                Show All Jobs
-              </Link>
-            </div>
-          )
-        }
-        
+        {ShowAllJobsBtn && (
+          <div className="flex justify-center items-center">
+            <Link
+              to="/jobs"
+              className="flex items-center gap-1 text-xl text-center"
+            >
+              <FaArrowAltCircleRight />
+              Show All Jobs
+            </Link>
+          </div>
+        )}
       </div>
     </section>
   );
