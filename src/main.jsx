9afdb59e027067a3904d8b6f2c0428d3309eebb1
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-04-03 11:36:48 +0100

    install tanstack/react-query and devtools for async state management and debugging

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,28 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from "./contextApi/authContext.jsx";
 import { Toaster } from "react-hot-toast";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")).render(
 
   
   <StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <App/>
       <Toaster />
       </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
